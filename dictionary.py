21ce986fcdfd902971dde9765406387880866a18
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-04-26 14:11:00 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,8 +3,13 @@ import json
 data=json.load(open("C:\\Users\\Snippy066\\Downloads\\data.json"))
 
 def definition(word):
+    word=word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     else:
         print("Sorry!! this word doesn't exist in dictionary...")
 
